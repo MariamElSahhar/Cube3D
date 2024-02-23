6605daf15cbdaa26261cd22d984452587bf12e88
MariamElSahhar <me1693@nyu.edu>
2024-02-23 17:29:05 +0400

    distance to wall

--- src/render.c
+++ src/render.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   render.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: melsahha <melsahha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: melsahha <melsahha@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 19:44:14 by melsahha          #+#    #+#             */
-/*   Updated: 2024/02/22 21:40:36 by melsahha         ###   ########.fr       */
+/*   Updated: 2024/02/23 17:28:00 by melsahha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,20 @@ double	h_intersect_dist(double alpha, t_map *map, t_player *player)
 	printf("getting horizontal line intersect\n");
 	y_incr = TILE;
 	x_incr = TILE / tan(alpha);
+	if (fabs(x_incr) >= (map->map_width * 25))
+		return (FLT_MAX);
 	y_first = player->pos[1] - (player->pos[1] % TILE);
 	x_first = ((player->pos[1] - y_first) / tan(alpha)) + player->pos[0];
 	double	i = 0;
-	while (!hit_wall(x_first + (x_incr * i), y_first + (y_incr * i), map))
+	while (floor((x_first + (x_incr * i)) / TILE) > 0
+		&& floor((x_first + (x_incr * i)) / TILE) < map->map_width
+		&& floor((y_first + (y_incr * i)) / 25) > 0
+		&& floor((y_first + (y_incr * i)) / 25) < map->map_height)
+	{
+		if(hit_wall(x_first + (x_incr * i), y_first + (y_incr * i), map))
+			return (sqrt(pow(x_first + x_incr * i, 2) + pow(y_first + y_incr * i, 2)));
 		i++;
+	}
 	printf("horizontal intersect at %f, %f\n", x_first + x_incr * i, y_first + y_incr * i);
 	return (sqrt(pow(x_first + x_incr * i, 2) + pow(y_first + y_incr * i, 2)));
 }
@@ -48,14 +57,23 @@ double	v_intersect_dist(double alpha, t_map *map, t_player *player)
 	printf("getting vertical line intersect\n");
 	x_incr = TILE;
 	y_incr = TILE * tan(alpha);
+	if (fabs(y_incr) >= (map->map_height * 25))
+		return (FLT_MAX);
 	x_first = player->pos[0] - (player->pos[0] % TILE);
 	y_first = ((player->pos[0] - x_first) * tan(alpha)) + player->pos[1];
 	printf("x_inc = %f, y_inc = %f, starting at (%f, %f)\n", x_incr, y_incr, x_first, y_first);
 	int	i = 0;
-	while (!hit_wall(x_first + (x_incr * i), y_first + (y_incr * i), map))
+	while (floor((x_first + (x_incr * i)) / TILE) > 0
+		&& floor((x_first + (x_incr * i)) / TILE) < map->map_width
+		&& floor((y_first + (y_incr * i)) / 25) > 0
+		&& floor((y_first + (y_incr * i)) / 25) < map->map_height)
+	{
+		if(hit_wall(x_first + (x_incr * i), y_first + (y_incr * i), map))
+			return (sqrt(pow(x_first + x_incr * i, 2) + pow(y_first + y_incr * i, 2)));
 		i++;
+	}
 	printf("vertical intersect at %f, %f\n", x_first + x_incr * i, y_first + y_incr * i);
-	return (sqrt(pow(x_first + x_incr * i, 2) + pow(y_first + y_incr * i, 2)));
+	return (FLT_MAX);
 }
 
 // void	render_wall(double alpha, int dist, t_map *map)
@@ -63,19 +81,15 @@ double	v_intersect_dist(double alpha, t_map *map, t_player *player)
 
 // }
 
-int	distance_to_wall(double alpha, t_map *map, t_player *player)
+double	distance_to_wall(double alpha, t_map *map, t_player *player)
 {
 	double	v_dist;
 	double	h_dist;
 	double	dist;
 
-	v_dist = FLT_MAX;
-	h_dist = FLT_MAX;
 	printf("getting distance to nearest wall alpha = %d\n", alpha == 0.0);
-	if (alpha != M_PI && fabs(alpha) > 0.0005)
-		v_dist = h_intersect_dist(alpha, map, player);
-	if (alpha != (M_PI / 2) && alpha != (3 * M_PI / 2))
-		h_dist = v_intersect_dist(alpha, map, player);
+	v_dist = h_intersect_dist(alpha, map, player);
+	h_dist = v_intersect_dist(alpha, map, player);
 	if (v_dist < h_dist)
 		dist = v_dist;
 	else
@@ -87,11 +101,10 @@ int	distance_to_wall(double alpha, t_map *map, t_player *player)
 
 void	cast_ray(int x, double alpha, t_data *data)
 {
-	int	dist;
+	double	dist;
 
-	printf("ray cast\n");
 	dist = distance_to_wall(alpha, &data->map, &data->player);
-	printf("distance to nearest wall is %i\n", dist);
+	printf("distance to nearest wall is %f\n", dist);
 	for (int i = 0; i < DIM_H; i++) {
 		put_pixel(&data->mlx, x, i, 0x4B0082);
 	}
@@ -113,14 +126,13 @@ void	put_map(t_data *data, t_map *map, t_player *player)
 	start_angle = player->alpha - ((FOV / 2) * (M_PI / 180));
 	end_angle = player->alpha + ((FOV / 2) * (M_PI / 180));
 	printf("increment %f, %f to %f\n", angle_increment, start_angle* (180 / M_PI), end_angle* (180 / M_PI));
-	// x = 0;
-	x = DIM_W / 2;
-	// while (x < DIM_W)
-	// {
-		// printf("ray #%d, angle = %f\n", x, normalize_angle(start_angle + (angle_increment * x)) * (180 / M_PI));
-		printf("casting %i\n", x);
+	x = 0;
+	// x = DIM_W / 2;
+	while (x < DIM_W)
+	{
+		printf("ray #%d, angle = %f\n", x, normalize_angle(start_angle + (angle_increment * x)) * (180 / M_PI));
 		cast_ray(x, start_angle + (angle_increment * x), data);
 		x ++;
-	// }
+	}
 	printf("\n");
 }
